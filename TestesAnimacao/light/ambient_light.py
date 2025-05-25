380713d4544a5a36d498cd5dacf592d6b78e3bac
TsukyamiT <t.teodoro2003@gmail.com>
2025-05-25 23:51:24 +0100

    mapa com harmonica

--- TestesAnimacao/light/ambient_light.py
+++ TestesAnimacao/light/ambient_light.py
@@ -1,8 +1,7 @@
-"""Implements the representation of ambient light"""
+from light.light import Light
 
-from py3d.light.light import Light
 
 class AmbientLight(Light):
-    def __init__(self, color = [1, 1, 1]):
+    def __init__(self, color=(1, 1, 1)):
         super().__init__(Light.AMBIENT)
-        self.color = color
+        self._color = color
